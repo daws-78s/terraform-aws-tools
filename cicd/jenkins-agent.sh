09427085c9b7bda3057f8b4c69e05edabe6525b4
joindevops <info@joindevops.com>
2024-07-16 09:00:04 +0530

    functions

--- cicd/jenkins-agent.sh
+++ cicd/jenkins-agent.sh
@@ -8,3 +8,16 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 yum install zip -y
+
+# docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+
+# Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
